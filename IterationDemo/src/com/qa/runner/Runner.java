0a0f2901bebfe1068291280f9b79e368be32a4a5
samkazshel <samsheldon779@gmail.com>
2021-10-07 14:12:58 +0100

    Working on iteration and loops in Java (For,While,DoWhile)

---
+++ IterationDemo/src/com/qa/runner/Runner.java
@@ -0,0 +1,15 @@
+package com.qa.runner;
+
+import com.qa.demo.IterationMethods;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		IterationMethods.whileDemo();
+//		IterationMethods.doWhileDemo();
+		IterationMethods.forLoopDemo(3000, 100);
+
+	}
+
+}
